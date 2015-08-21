b75504889e80e350dd6b90d043c7c6b8311e8e7d
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-08-21 17:06:43 -0500

    Added function for mapping transformations over event sequences where both input and output are event streams.
    * Added corresponding tests.

--- esal/streams.py
+++ esal/streams.py
@@ -10,6 +10,7 @@ import itertools as itools
 # Export public API
 __all__ = (
     'collect_event_sequences',
+    'map_sequences_as_events',
     'select',
     )
 
@@ -32,6 +33,32 @@ def collect_event_sequences(events):
         # Keep event sequences as iterables rather than instantiating to
         # any particular collection
         yield sequence
+
+def map_sequences_as_events(function, events):
+    """Maps the given function over event sequences given an event
+    stream and returns an event stream.
+
+    This function is intended for transforming event sequences into
+    other event sequences when both the input and output are event
+    streams.  It collects a stream of events into event sequences,
+    applies the given function to each event sequence, and then flattens
+    the result of the function back into a stream of events.  This is
+    conceptually equivalent to
+
+    >>> flatten(map(function, collect_sequences(events)))
+
+    Flattening only occurs when the given function returns an iterable.
+    If you want to map over sequences without flattening, just use
+
+    >>> map(function, collect_sequences(events))
+    """
+    for _, sequence in itools.groupby(events, key=_seq_id):
+        result = function(sequence)
+        if hasattr(result, '__iter__'):
+            for event in function(sequence):
+                yield event
+        else:
+            yield result
 
 def select(events, field, values):
     """Selects events where the value of the indicated field is in the
--- esal/test/streams_test.py
+++ esal/test/streams_test.py
@@ -3,10 +3,12 @@
 # Copyright (c) 2015 Aubrey Barnard.  This is free software.  See
 # LICENSE for details.
 
+import itertools as itools
 import unittest
 
-from .. import streams
 from . import data
+from .. import engine
+from .. import streams
 
 
 class EventStreamOperationsTest(unittest.TestCase):
@@ -20,6 +22,28 @@ class EventStreamOperationsTest(unittest.TestCase):
         actual = tuple(
             tuple(seq) for seq in
             streams.collect_event_sequences(data.simple_events))
+        self.assertEqual(expected, actual)
+
+    def test_map_sequences_as_events_to_values(self):
+        """Tests mapping sequences to values."""
+        expected = (7, 3, 14)
+        actual = tuple(streams.map_sequences_as_events(
+                engine.count, data.simple_concurrent_events))
+        self.assertEqual(expected, actual)
+
+    def test_map_sequences_as_events_to_sequences(self):
+        """Tests mapping sequences to sequences."""
+        def first3(iterable):
+            for idx, item in enumerate(iterable):
+                yield item
+                if idx >= 2:
+                    break
+        expected = list(itools.chain(
+                data.simple_concurrent_events[0:3],
+                data.simple_concurrent_events[7:10],
+                data.simple_concurrent_events[10:13]))
+        actual = list(streams.map_sequences_as_events(
+                first3, data.simple_concurrent_events))
         self.assertEqual(expected, actual)
 
     def test_select_events(self):
