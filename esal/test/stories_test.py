39f09876ddbdbbf4d1d8daa622c16754d768e063
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-08-21 17:16:22 -0500

    Finished implementing story 004.
    * Added test for story 004.

--- esal/test/stories_test.py
+++ esal/test/stories_test.py
@@ -5,8 +5,11 @@
 
 import unittest
 
-from .. import engine
 from . import data
+from .. import engine
+from .. import events
+from .. import sequences
+from .. import streams
 
 
 # Selection predicates
@@ -52,3 +55,28 @@ class StoryTests(unittest.TestCase):
                              engine.select(
                                  data.med_events, _is_unobserved),
                              (3,)))))
+
+    def test_story004(self):
+        # Order events in reverse by their name and then strip times to
+        # convert to sequences
+        def reverse_events_to_sequences(seq):
+            return sequences.timelines_to_sequences(
+                sequences.order_concurrent_events(
+                    seq,
+                    ordering=lambda evs: sorted(
+                        evs, key=lambda e: e.ev, reverse=True)))
+        ev_idxs = (
+            (1, 0, 3, 2, 6, 5, 4), # Seq 0
+            (9, 8, 7), # Seq 1
+            (12, 11, 10, 15, 14, 13, 18, 17, 16, 20, 19, 23, 22, 21), # Seq 2
+            )
+        expected = []
+        for seq in ev_idxs:
+            for idx, ev_idx in enumerate(seq):
+                ev = data.simple_concurrent_events[ev_idx]
+                expected.append(events.Event(
+                        ev.seq, idx, ev.dura, ev.ev, ev.val))
+        actual = list(streams.map_sequences_as_events(
+                reverse_events_to_sequences,
+                data.simple_concurrent_events))
+        self.assertEqual(expected, actual)
