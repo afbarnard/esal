44a29c6efaef264bb602b45efcb9bb6f7b488af9
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-09-01 18:05:43 -0500

    Implemented getting items from an iterable by index.
    * Added general function 'getitems' which relies on two support
      functions, one for each type of input: indexable and iterable.
    * Wrote corresponding tests.
    * Updated TODOs in todo.txt.

--- esal/general.py
+++ esal/general.py
@@ -3,10 +3,13 @@
 # Copyright (c) 2015 Aubrey Barnard.  This is free software.  See
 # LICENSE for details.
 
+import itertools as itools
+
+
 def firsts(items, key=None):
     """Generates the first occurrence of each unique item.
 
-    * items: Iterable of items
+    * items: Iterable of items.
     * key: Function computing the key for each item.  The key determines
       whether an item is unique.  If key is 'None', the item is its own
       key.
@@ -19,3 +22,58 @@ def firsts(items, key=None):
         if k not in seen:
             seen.add(k)
             yield item
+
+def _getitems_indexable(indexable_items, indices):
+    if indexable_items: # __bool__ calls __len__, which was assumed
+        for idx in indices:
+            yield indexable_items[idx]
+
+def _getitems_iterable(iterable_items, indices):
+    # This is basically a one-sided merge join algorithm when given
+    # sorted indices.  It is one-sided in the sense that the item index
+    # will keep increasing and will not be repeated but some of the
+    # given indices may be repeated.
+
+    # Sort the indices in a way that notes the original order
+    sorted_pairs = sorted(zip(indices, itools.count()))
+    # Return if the indices are empty
+    if not sorted_pairs:
+        return
+    # Setup
+    item_iter = iter(iterable_items)
+    item_idx = 0
+    items = [None] * len(sorted_pairs)
+    # Catch iteration termination
+    try:
+        item = next(item_iter)
+        # Get an item (by index) for each of the specified indices
+        for index, order in sorted_pairs:
+            while item_idx < index:
+                item = next(item_iter)
+                item_idx += 1
+            items[order] = item
+    except StopIteration:
+        pass
+    # Return if the iterable is empty
+    if item_idx == 0:
+        return
+    # Generate the items
+    for item in items:
+        yield item
+
+def getitems(items, indices):
+    """Generates the items at the given indices.
+
+    * items: Iterable of items.
+    * indices: Iterable of indices of the items to return.
+
+    If 'items' is indexable then the items are accessed by index.
+    Otherwise, if 'items' is only iterable, then the indices are joined
+    against the items.  The join algorithm expects that there are many
+    more items than indices, and so uses memory proportional to the
+    number of indices.
+    """
+    if hasattr(items, '__getitem__') and hasattr(items, '__len__'):
+        return _getitems_indexable(items, indices)
+    else:
+        return _getitems_iterable(items, indices)
--- esal/test/general_test.py
+++ esal/test/general_test.py
@@ -7,6 +7,7 @@ import unittest
 
 from .. import general
 
+
 class FirstsTest(unittest.TestCase):
 
     def test_empty(self):
@@ -30,4 +31,56 @@ class FirstsTest(unittest.TestCase):
             )
         expected = ((5, 'a'), (5, 'b'), (1, 'c'))
         actual = tuple(general.firsts(pairs, key=lambda p: p[1]))
+        self.assertEqual(expected, actual)
+
+
+class GetItemsTest(unittest.TestCase):
+
+    items = (
+        15, 14, 5, 9, 2,
+        1, 9, 15, 5, 9,
+        1, 9, 13, 10, 5,
+        8, 16, 2, 16, 18,
+        )
+    indices1 = (0, 14, 1, 13, 8, 5, 5)
+    items1 = (15, 5, 14, 10, 5, 1, 1)
+    indices2 = (0, 4, 12, 17, 18, 19, 19)
+    items2 = (15, 2, 13, 2, 16, 18, 18)
+
+    def test_empty_items_indexable(self):
+        expected = ()
+        actual = tuple(general.getitems((), iter((1, 2, 3))))
+        self.assertEqual(expected, actual)
+
+    def test_empty_items_iterable(self):
+        expected = ()
+        actual = tuple(general.getitems(iter(()), iter((1, 2, 3))))
+        self.assertEqual(expected, actual)
+
+    def test_empty_indices_indexable(self):
+        expected = ()
+        actual = tuple(general.getitems(self.items, iter(())))
+        self.assertEqual(expected, actual)
+
+    def test_empty_indices_iterable(self):
+        expected = ()
+        actual = tuple(general.getitems(iter(self.items), iter(())))
+        self.assertEqual(expected, actual)
+
+    def test_indexable_items(self):
+        expected = self.items1
+        actual = tuple(general.getitems(
+                self.items, iter(self.indices1)))
+        self.assertEqual(expected, actual)
+
+    def test_iterable_items_sorted_indices(self):
+        expected = self.items2
+        actual = tuple(general.getitems(
+                iter(self.items), iter(self.indices2)))
+        self.assertEqual(expected, actual)
+
+    def test_iterable_items_unsorted_indices(self):
+        expected = self.items1
+        actual = tuple(general.getitems(
+                iter(self.items), iter(self.indices1)))
         self.assertEqual(expected, actual)
