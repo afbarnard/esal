e1d06c79d3ca8b30e5a728a16172c5798e508987
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-09-29 17:22:34 -0500

    Refactored Event into a proper class.
    * Converted Event from a namedtuple with methods tacked on to a subclass
      of a namedtuple.  This makes Event cleaner, prettier, and easier to
      extend in the future.
    * Added Event tests to cover all the desired properties described in
      story 007.
    * Reordered Header and Event classes because Event now depends on
      Header.  Same for their tests.
    * Added 'names' property to Header.
    * Added 'header' property to Event.
    * Removed EVENT_HEADER and EVENT_FIELD_NAMES in favor of 'header' and
      'header.names' properties of Event.
    * Removed Header.value_of method (and its test) as it didn't seem very
      useful.

--- esal/test/events_test.py
+++ esal/test/events_test.py
@@ -9,26 +9,6 @@ from .. import events
 from .. import general
 
 
-class EventTest(unittest.TestCase):
-
-    event_fields = ('pt12345', '2015-04-28', 31, 'mi', True)
-
-    def setUp(self):
-        self.event = events.Event(*self.event_fields)
-
-    def test_getitem_by_index(self):
-        for index, value in enumerate(self.event_fields):
-            self.assertEqual(value, self.event[index])
-
-    def test_getitem_by_name(self):
-        for index, name in enumerate(self.event._fields):
-            self.assertEqual(self.event_fields[index], self.event[name])
-
-    def test_sort_key(self):
-        key = general.iterable_sort_key(self.event_fields)
-        self.assertEqual(key, self.event.sort_key())
-
-
 class HeaderTest(unittest.TestCase):
 
     names = ('kew', 'peyo', 'sef', 'ster', 'nati', 'umb', 'roge')
@@ -52,7 +32,72 @@ class HeaderTest(unittest.TestCase):
         for index, name in enumerate(self.names):
             self.assertEqual(index, self.header.index_of(name))
 
-    def test_value_of(self):
-        for index, name in enumerate(self.names):
-            self.assertEqual(name, self.header.value_of(self.names, name))
-            self.assertEqual(name, self.header.value_of(self.names, index))
+
+class EventTest(unittest.TestCase):
+
+    """Thoroughly tests Event according to the desired properties listed
+    in story 007.
+    """
+
+    event_fields = ('pt12345', '2015-04-28', 31, 'mi', True)
+
+    def setUp(self):
+        self.event = events.Event(*self.event_fields)
+
+    def test_construction_by_name_value_pairs(self):
+        self.event = events.Event(time=3.3, ev='c', seq=111)
+        self.assertEqual(111, self.event.seq)
+        self.assertEqual(3.3, self.event.time)
+        self.assertEqual('c', self.event.ev)
+        self.assertIsNone(self.event.dura)
+        self.assertIsNone(self.event.val)
+
+    def test_access_field_by_index(self):
+        for index, value in enumerate(self.event_fields):
+            self.assertEqual(value, self.event[index])
+
+    def test_access_field_by_name(self):
+        for index, name in enumerate(self.event.header.names):
+            self.assertEqual(self.event_fields[index], self.event[name])
+
+    def test_access_field_by_attribute(self):
+        for index, name in enumerate(self.event.header.names):
+            self.assertEqual(
+                self.event_fields[index], getattr(self.event, name))
+
+    def test_read_only_attributes(self):
+        for index, name in enumerate(self.event.header.names):
+            with self.assertRaises(AttributeError):
+                setattr(self.event, name, index)
+
+    def test_read_only_items(self):
+        for index, name in enumerate(self.event.header.names):
+            with self.assertRaises(TypeError):
+                self.event[index] = name
+
+    def test_len(self):
+        self.assertEqual(5, len(self.event))
+
+    def test_iter(self):
+        for idx, field in enumerate(self.event):
+            self.assertEqual(self.event_fields[idx], field)
+
+    def test_contains(self):
+        for field in self.event_fields:
+            self.assertIn(field, self.event)
+
+    def test_eq(self):
+        event2 = events.Event(*self.event_fields)
+        self.assertEqual(self.event, event2)
+
+    def test_hash(self):
+        event2 = events.Event(*self.event_fields)
+        self.assertEqual(hash(self.event), hash(event2))
+
+    def test_repr(self):
+        rep = "Event(seq='pt12345', time='2015-04-28', dura=31, ev='mi', val=True)"
+        self.assertEqual(rep, repr(self.event))
+
+    def test_sort_key(self):
+        key = general.iterable_sort_key(self.event_fields)
+        self.assertEqual(key, self.event.sort_key())
