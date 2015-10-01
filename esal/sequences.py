d1e67b06abc2f27fa6ddeaac12c7a9dd2d973a01
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-09-30 20:43:21 -0500

    Implemented EventSequence according to design in story 006.
    * Created EventSequence class to be a collection of Events in temporal
      order.  This required devising universal sort keys for tuples since
      sorting (comparing for order) with None in Python 3 gives a type error
      (but worked in Python 2).
    * Implemented Sequence API so that EventSequence can be used like any
      other Python collection.
    * Wrote corresponding tests.
    * Required implementing Event matching.

--- esal/sequences.py
+++ esal/sequences.py
@@ -4,10 +4,12 @@
 # Copyright (c) 2015 Aubrey Barnard.  This is free software.  See
 # LICENSE for details.
 
+import collections.abc
 import itertools as itools
 import random
 
 from . import events
+from .general import Any
 
 
 # Export public API
@@ -85,18 +87,61 @@ def make_timeline_to_sequence_flattener(ordering=data_order, start=0):
     return flattener
 
 
-# Event sequence objects
+class EventSequence(collections.abc.Sequence):
+    """A collection of events in temporal order."""
 
-class _EventSequence(object): # TODO?
-    """A sequence of events."""
+    def __init__(self, events):
+        self.events = sorted(events, key=lambda e: e.sort_key())
 
-    def __init__(self, sequence_id, events):
-        # Initialize members
-        self._seq_id = sequence_id
-        self._events = []
-        self._times = None
-        self._duras = None
-        self._values = None
-        # Set members from 'events' depending on type
-        for idx, event in enumerate(events):
-            pass
+    def __len__(self):
+        return len(self.events)
+
+    def __getitem__(self, key):
+        return self.events[key]
+
+    def __contains__(self, obj):
+        """Returns whether this event sequence contains an event with
+        the given value of the named field or whether this event
+        sequence contains the given object.
+
+        * obj: A (field_name::str, value) pair or some object (typically
+          an Event).  If 'obj' is a (::str, ::object) pair, this
+          searches for an Event with a field with the given value.
+          Otherwise, this just searches for the given object (which must
+          be an Event to succeed).
+        """
+        if (isinstance(obj, tuple) and len(obj) == 2 and
+                isinstance(obj[0], str)):
+            field, value = obj
+            return value in (ev[field] for ev in self.events)
+        else:
+            return obj in self.events
+
+    def times(self):
+        """Returns the times of the events in sequence order."""
+        yield from (ev.time for ev in self.events)
+
+    def types(self):
+        """Returns the types of the events in sequence order."""
+        yield from (ev.ev for ev in self.events)
+
+    def match(self, seq=Any, time=Any, dura=Any, ev=Any, val=Any,
+              start=0, stop=None):
+        """Returns the index of the first event that matches the given
+        field values.
+
+        Matching works as for Event.matches.  Providing 'start' and
+        'stop' limits the search to that slice, like for list.index.
+        """
+        stop = len(self.events) if stop is None else stop
+        for idx in range(start, stop):
+            if self.events[idx].matches(seq, time, dura, ev, val):
+                return idx
+        return None
+
+    def matches(self, seq=Any, time=Any, dura=Any, ev=Any, val=Any):
+        """Returns the indices of all the matches that would be returned
+        by successive calls to 'match'.
+        """
+        yield from (idx for idx, event in enumerate(self.events)
+                    if event.matches(seq, time, dura, ev, val))
