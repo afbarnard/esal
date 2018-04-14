ed1aeb4c1c73ce4ffb8ce7ab3f12113cdd15f7e3
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2018-04-14 15:23:46 -0500

    Created `setup.py` to enable installation.
    * Created `setup.py` based on the one from `barnapy`.
    * Updated package docstring to include basic information from README.
    * Added version number.
    * Bumped copyright year.

--- esal/__init__.py
+++ esal/__init__.py
@@ -1,7 +1,27 @@
-# Package definition and setup
-#
-# Copyright (c) 2015 Aubrey Barnard.  This is free software.  See
-# LICENSE for details.
+"""
+Event Sequence Analysis Library: statistical analysis and
+manipulation of event sequence data
+
+Esal ("easel") is a library for the descriptive statistical analysis and
+manipulation of event sequences and timelines.  Esal is intended to be
+used for exploring event sequence data and preparing data for modeling,
+but does not do any modeling itself.  Conceptually, Esal is a
+representation for a dataset of sequences/timelines and an associated
+set of meaningful operations (selection, counting, transformation).
+
+The core concept is an event, which can be though of as a 5-tuple
+(sequence-id, start-time, stop-time, event-type, value).  The stop time
+and value are optional.
+
+Copyright (c) 2018 Aubrey Barnard.  This is free software.  See LICENSE
+for details.
+"""
+# The above text is used by `setup.py`.
+
+
+# Version
+__version__ = '0.1.0'
+
 
 # Expose core API at the top level
 from .events import *
---
+++ setup.py
@@ -0,0 +1,60 @@
+"""Esal package definition and install configuration"""
+
+# Copyright (c) 2018 Aubrey Barnard.  This is free software released
+# under the MIT license.  See `LICENSE` for details.
+
+
+import setuptools
+
+import esal
+
+
+# Extract the short and long descriptions from the documentation
+_desc_paragraphs = esal.__doc__.strip().split('\n\n')
+# Make sure to keep the short description to a single line
+_desc_short = _desc_paragraphs[0].replace('\n', ' ')
+# Include all the package documentation in the long description except
+# for the first and last paragraphs which are the short description and
+# the copyright notice, respectively
+_desc_long = '\n\n'.join(_desc_paragraphs[1:-1])
+
+
+# Define package attributes
+setuptools.setup(
+
+    # Basics
+    name='esal',
+    version=esal.__version__,
+    url='https://github.com/afbarnard/esal',
+    license='MIT',
+    author='Aubrey Barnard',
+    #author_email='',
+
+    # Description
+    description=_desc_short,
+    long_description=_desc_long,
+    keywords=[
+        'event sequences',
+        'data processing',
+        'data analysis',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    platforms=['any'],
+
+    # Requirements
+    python_requires='>=3.4',
+    install_requires=[], # No dependencies
+
+    # API
+    packages=setuptools.find_packages(),
+
+)
